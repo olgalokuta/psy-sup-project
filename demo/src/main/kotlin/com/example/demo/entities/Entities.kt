403354eeb1462b2b8f1ed5af63ba2ece59660cc1
mariibykova <101357988+mariibykova@users.noreply.github.com>
2024-01-20 12:43:23 +0400

    Добавила сущности Topics, Status и UserStatus
    Добавила сущности Topics, Status и UserStatus

--- demo/src/main/kotlin/com/example/demo/entities/Entities.kt
+++ demo/src/main/kotlin/com/example/demo/entities/Entities.kt
@@ -48,3 +48,35 @@ data class Comment(
     val idanscomment: Int
 )
 
+@Entity
+@Table(name = "topics")
+data class Topic(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int,
+    val topic: String
+)
+
+@Entity
+@Table(name = "status")
+data class Status(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int,
+    val status: Boolean
+)
+
+@Entity
+@Table(name = "userstatus")
+data class UserStatus(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int,
+    val iduser: Int,
+    val idstatus Int,
+    val timebeg: LocalDateTime,
+    val timeend: LocalDateTime
+)
+
+
+
