755701585db7887e9d0ab9a07102717fe589f68e
mariibykova <101357988+mariibykova@users.noreply.github.com>
2024-06-13 21:28:25 +0400

    Изменение в связи с добавлением картинки

--- demo/src/main/kotlin/com/example/demo/controller/CommentController.kt
+++ demo/src/main/kotlin/com/example/demo/controller/CommentController.kt
@@ -8,7 +8,6 @@ import com.example.demo.repositories.CommentRepository
 import com.example.demo.models.Comment
 
 @RestController
-@CrossOrigin(origins=["http://localhost:3000", "http://62.3.58.13"])
 @RequestMapping("/api/comments")
 class CommentController(@Autowired private val commentRepository: CommentRepository) {
 
@@ -19,22 +18,6 @@ class CommentController(@Autowired private val commentRepository: CommentReposit
     @GetMapping("/entry/{eid}")
     fun getAllEntryComments(@PathVariable("eid") entryId: Int): List<Comment> =
         commentRepository.findByIdentry(entryId).toList()
-
-    @GetMapping("/formoderation/{id}")
-    fun getUnmoderatedComments(@PathVariable("id") modId: Int): ResponseEntity<Comment?> { 
-        val unfinished = commentRepository.findByModeratorAndModerated(modId, false)
-        if (unfinished.size > 0) return ResponseEntity(unfinished[0], HttpStatus.OK)
-        
-        val unmod = commentRepository.findByModeratedOrderByPostedAsc(false).toList()
-        for (e in unmod) {
-            if (e.moderator == null) {
-                val upd = e.copy(moderator = modId)
-                commentRepository.save(upd)
-                return ResponseEntity(upd, HttpStatus.OK)
-            }
-        }
-        return ResponseEntity(HttpStatus.NO_CONTENT)
-    }
 
     @PostMapping("")
     fun createComment(@RequestBody comment: Comment): ResponseEntity<Comment> {
@@ -60,7 +43,7 @@ class CommentController(@Autowired private val commentRepository: CommentReposit
 
         val updatedComment = existingComment.copy(iduser = comment.iduser, posted = comment.posted, 
             content = comment.content, moderated = comment.moderated, identry = comment.identry, 
-            idanscomment = comment.idanscomment)
+            idanscomment = comment.idanscomment,  image = comment.image)
         commentRepository.save(updatedComment)
         return ResponseEntity(updatedComment, HttpStatus.OK)
     }
