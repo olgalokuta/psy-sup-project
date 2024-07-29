735c446c50450dfa0b70f9b65158aac15d2bbcfd
mariibykova <101357988+mariibykova@users.noreply.github.com>
2024-07-29 12:32:14 +0700

    Отображение картинок в ленте

--- PsySupApplication/app/src/main/java/com/example/psysupapplication/EntryView.kt
+++ PsySupApplication/app/src/main/java/com/example/psysupapplication/EntryView.kt
@@ -1,6 +1,10 @@
 package com.example.psysupapplication
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,6 +15,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -20,19 +25,35 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.psysupapplication.ui.theme.PurpleGrey80
+
+
 @Composable
 fun EntryView(user : User, entry : Entry, isEditing : MutableState<Boolean>,
               isCommenting : MutableState<Boolean>,
               currentEntry: MutableState<Entry?>,
               inProfile: Boolean) : Unit {
+    val photos = remember {
+        if (entry.photos == null) {
+            listOf()
+        } else {
+            entry.photos.map {
+                val bytes = Base64.decode(it, Base64.DEFAULT)
+                BitmapFactory.decodeByteArray(bytes, 0, bytes.size).asImageBitmap()
+            }.toList()
+        }
+    }
+
     Card(
         modifier = Modifier.fillMaxWidth(),
         shape = MaterialTheme.shapes.medium,
@@ -45,7 +66,9 @@ fun EntryView(user : User, entry : Entry, isEditing : MutableState<Boolean>,
                 Image(
                     painter = painterResource(id = R.drawable.default_avatar),
                     contentDescription = "Default avatar",
-                    modifier = Modifier.size(42.dp).clip(CircleShape)
+                    modifier = Modifier
+                        .size(42.dp)
+                        .clip(CircleShape)
                 )
 
                 Spacer(modifier = Modifier.width(15.dp))
@@ -100,6 +123,18 @@ fun EntryView(user : User, entry : Entry, isEditing : MutableState<Boolean>,
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Medium
             )
+            if (photos.isNotEmpty()) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .horizontalScroll(rememberScrollState()),
+                    horizontalArrangement = Arrangement.spacedBy(20.dp)
+                ) {
+                    for (photo in photos) {
+                        Image(photo, "ASD")
+                    }
+                }
+            }
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.End
@@ -129,7 +164,9 @@ fun StaticEntry (user: User, entry : Entry) : Unit {
                 Image(
                     painter = painterResource(id = R.drawable.default_avatar),
                     contentDescription = "Default avatar",
-                    modifier = Modifier.size(42.dp).clip(CircleShape)
+                    modifier = Modifier
+                        .size(42.dp)
+                        .clip(CircleShape)
                 )
 
                 Spacer(modifier = Modifier.width(15.dp))
