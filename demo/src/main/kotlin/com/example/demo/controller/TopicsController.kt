01467f0fbc4320a4bee867fdee4ecdcaf481a49f
anasybll <anasty_b@mail.ru>
2024-10-04 19:05:57 +0700

    Добавление стандартных тем при запуске сервера

--- demo/src/main/kotlin/com/example/demo/controller/TopicsController.kt
+++ demo/src/main/kotlin/com/example/demo/controller/TopicsController.kt
@@ -12,6 +12,15 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/topics")
 class TopicsController(@Autowired private val topicsRepository: TopicsRepository) {
+    init {
+        // Если тем нет, то вставляются стандартные
+        if (topicsRepository.count() == 0.toLong()) {
+            topicsRepository.save(Topic(id = 0, topic = "Психология"))
+            topicsRepository.save(Topic(id = 1, topic = "Рассуждения"))
+            topicsRepository.save(Topic(id = 2, topic = "Разное"))
+        }
+    }
+
     @GetMapping("")
     fun getAllTopics(): ResponseEntity<List<Topic>> =
         ResponseEntity(topicsRepository.findAll().toList(), HttpStatus.OK) // Получение всех тем
--- demo/src/main/kotlin/com/example/demo/repositories/EntryRepository.kt
+++ demo/src/main/kotlin/com/example/demo/repositories/EntryRepository.kt
@@ -3,10 +3,14 @@ package com.example.demo.repositories
 import org.springframework.data.repository.CrudRepository
 import com.example.demo.models.Entry
 import com.example.demo.models.Visibility
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface EntryRepository : CrudRepository<Entry, Int> {
     fun findByIduser(userId : Int) : List <Entry>
     fun findByVisibility(visibility : Visibility) : List <Entry>
+    @Query(value = "select * from entries where visibility = 0 and :topicId = any(topics)", nativeQuery = true) // Получить публичные посты с темой topicId
+    fun getPublicWithTopic(@Param("topicId") topicId: Int) : List<Entry>
     // fun findByVisibilityAndModeratedOrderByPostedDesc(pub : Visibility, mod: Boolean) : List <Entry>
     // fun findByVisibilityAndModeratedOrderByPostedAsc(pub : Visibility, mod: Boolean) : List <Entry>
     // fun findByModeratorAndModerated(modId: Int, finished: Boolean): List<Entry>
